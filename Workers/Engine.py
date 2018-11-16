3661ea73f74c5411873c2dda87344252f63b9f7d
David Beck <davidernestbeck@gmail.com>
2018-11-15 18:32:41 -0600

    Added simple recommender. Still need to preprocess data more thoroughly.

--- Workers/Engine.py
+++ Workers/Engine.py
@@ -22,13 +22,33 @@ class Engine:
         # collects user data from Window class
         profile = self.user_handler.get_profile(user, preferences)
 
-    def apply_knn(self):
+    def apply_knn(self, title):
         # where the magic happens
         # should return a predicted class that can be used to recall
         # recommended movies from the data set
+        ## MAY need to transform to np.array
+        #neigh = knn.NearestNeighbors(n_neighbors=10)
+        #neigh.fit(self.movie_data.data[])
+        #get index of k nearest neighbors
+        #neigh.kneighbors(title, return_distance=False)
         pass
 
 
+    def get_recommendations(self, title):
+        # Get the index of the movie that matches the title
+        idx = self.movie_data.indices[title]
+        # Get cosine similarity matrix
+        cosine_sim = self.movie_data.cosine_sim
+        # Get the pairwise similarity scores of all movies with that movie
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        # Sort the movies based on the similarity scores
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        # Get the scores of the 10 most similar movies
+        sim_scores = sim_scores[1:11]
+        # Get the movie indices
+        movie_indices = [i[0] for i in sim_scores]
+        # Return the top 10 most similar movies
+        return self.movie_data.data['title'].iloc[movie_indices]
 
 
 
--- Workers/MovieData.py
+++ Workers/MovieData.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 import os
 
 # Should split the data into sets in this class
@@ -15,14 +17,29 @@ class MovieData:
         # TODO: call preprocess to get target variable from data
         # self.datafile could be split into target and data
         # should probably change with each search
-        self.data = 'This variable should contain the movie data'
+        self.data = self.datafile.copy()
         self.target = 'This variable should contain the target data'
+        # Construct a reverse map of indices and movie titles
+        self.indices = pd.Series(self.data.index, index=self.data['title']).drop_duplicates()
+        # Below could be target if we go for most similar as only feature.
+        self.cosine_sim = self.data.preprocess()
+
+    def __getattr__(self, item):
+        return "Attribute does not exist."
 
     def preprocess(self):
-        # TODO: at least get target variables from data, but clean if necessary
+        # TODO: at least get target variables from data, but clean if necessary & reduce dimension size
         # have to normalize all this crap: get_dummies()?
-        # what does preprocessing mean for this data?
-        pass
+        # Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+        tfidf = TfidfVectorizer(stop_words='english')
+        # Replace NaN with an empty string
+        self.data['overview'] = self.data['overview'].fillna('')
+        # Construct the required TF-IDF matrix by fitting and transforming the data
+        tfidf_matrix = tfidf.fit_transform(self.data['overview'])
+        # Compute the cosine similarity matrix
+        cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+        return cosine_sim
+
 
     def reassign_target(self, new_target):
         # new_target should be a column name in the movie data set
