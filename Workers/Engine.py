f55f338fa037f93bbe57a478890bc3b7355bed1a
David Beck <davidernestbeck@gmail.com>
2018-11-16 21:23:16 -0600

    blah

--- Workers/Engine.py
+++ Workers/Engine.py
@@ -45,21 +45,29 @@ class Engine:
         #print(movie_indices)
         return self.movie_data.data['title'].iloc[kneighbors]
 
+    # NOTE: This shit runs but it sucks at making predictions...
     def get_content_recommendations(self, title):
         # Get the index of the movie that matches the title
         idx = self.movie_data.data.title[self.movie_data.data.title == title].index
         # Get cosine similarity matrix from MovieData
         cosine_sim = self.movie_data.cosine_sim
         # Get the pairwise similarity scores of all movies with that movie
-        sim_scores = list(enumerate(cosine_sim[idx]))
+        print(cosine_sim[idx])
+        #sim_scores = list(enumerate(cosine_sim[idx]))
         # Sort the movies based on the similarity scores
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        #sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = np.flip(np.argsort(cosine_sim[idx]))
         # Get the scores of the 10 most similar movies
-        sim_scores = sim_scores[1:11]
+        print(sim_scores)
+        print(sim_scores.shape)
+        sim_scores = sim_scores[0, 1:11]
+        print(sim_scores)
         # Get the movie indices
-        movie_indices = [i[0] for i in sim_scores]
+        #movie_indices = [i[0] for i in sim_scores]
+        #print(movie_indices)
         # Return the top 10 most similar movies
-        return self.movie_data.data['title'].iloc[movie_indices]
+        return self.movie_data.data['title'].iloc[sim_scores]
+
 
     def get_top_movies(self, n):
         # Sort movies based on weighted score
