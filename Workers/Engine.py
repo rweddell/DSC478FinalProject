2b87a4a0e97abb47695bdce8a6cb1f1432936bf0
Rob Weddell <rweddell4@gmail.com>
2018-11-17 16:59:13 -0600

    Update Engine.py

--- Workers/Engine.py
+++ Workers/Engine.py
@@ -44,6 +44,22 @@ class Engine:
         # Return the top 10 most similar movies
         return self.movie_data.data['title'].iloc[sim_scores]
 
+    def get_rating_recommendations(self, title):
+        # Get the index of the movie that matches the title
+        idx = self.movie_data.data.title[self.movie_data.data.title == title].index
+        # Get cosine similarity matrix from MovieData
+        cosine_sim = self.movie_data.cosine_sim
+        # Get the pairwise similarity scores of all movies with that movie
+        #sim_scores = list(enumerate(cosine_sim[idx]))
+        # Sort the movies based on the similarity scores
+        #sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = np.flip(np.argsort(cosine_sim[idx]))
+        # Get the scores of the 10 most similar movies
+        sim_scores = sim_scores[0, 1:11]
+        # Return the top 10 most similar movies
+        return self.movie_data.data['title'].iloc[sim_scores]
+
+
     def get_top_movies(self, n):
         # Sort movies based on weighted score
         top_movies = self.movie_data.data.sort_values('score', ascending=False)
