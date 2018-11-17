f6e085a474856fa81e9107cf942097d5890d8cae
David Beck <davidernestbeck@gmail.com>
2018-11-17 17:00:30 -0600

    Improvements!

--- Workers/MovieData.py
+++ Workers/MovieData.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from zipfile import ZipFile
 from ast import literal_eval
+from nltk.stem.snowball import SnowballStemmer
 import os
 
 
@@ -47,23 +48,42 @@ class MovieData:
         # Merge keywords into dataframe
         data = data.merge(keywords, on='id')
         data['keywords'] = data['keywords'].apply(literal_eval)
+        # Strip out 'name' from keywords
         data['keywords'] = data['keywords'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
-        #links_small = pd.read_csv(os.path.join(self.data_path, 'links_small.csv'), low_memory=False)
-        #links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
-        #data = data[data['id'].isin(links_small)]
-        # print(data.shape)
-        data['tagline'] = data['tagline'].fillna('')
-        data['description'] = data['overview'] + data['tagline'] + data['keywords']
-        data['description'] = data['description'].fillna('')
-        # Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
-        tfidf = TfidfVectorizer(stop_words='english')
+        # Remove keywords that occur only once
+        s = data.apply(lambda x: pd.Series(x['keywords']), axis=1).stack().reset_index(level=1, drop=True)
+        s.name = 'keyword'
+        s = s.value_counts()
+        d = s[s == 1]
+        data['keywords'] = data['keywords'].apply(lambda x: (i for i in x if i not in d))
         # Replace NaN with an empty string
         data['overview'] = data['overview'].fillna('')
+        data['tagline'] = data['tagline'].fillna('')
+        # see about genre...
+        data['genres'] = data['genres'].fillna('[]').apply(literal_eval).apply(
+            lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+        # Convert values to strings for concatenation
+        data['keywords'] = data['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+        data['tagline'] = data['tagline'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+        data['overview'] = data['overview'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+        # Stem
+        data['wordsalad'] = data['overview'] + data['tagline'] + data['keywords'] + data['genres']
+        data['wordsalad'] = data['wordsalad'].fillna('')
+        data['wordsalad'] = data['wordsalad'].apply(lambda x: ' '.join(x))
+        # Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+        tfidf = TfidfVectorizer(stop_words='english')
         # Construct the required TF-IDF matrix by fitting and transforming the data
-        tfidf_matrix = tfidf.fit_transform(data['overview'])
+        tfidf_matrix = tfidf.fit_transform(data['wordsalad'])
         # Compute the cosine similarity matrix
         cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
         return data, cosine_sim, tfidf_matrix
+
+    def filter_keywords(self, s):
+        words = []
+        for i in self:
+            if i in s:
+                words.append(i)
+        return words
 
     def data_unzip(self):
         zip_ref = ZipFile(os.path.join(self.data_path, 'movies_metadata.zip'), 'r')
