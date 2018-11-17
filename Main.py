df17ad125f2911942a0324fbe9ef551f8ee363cd
Rob Weddell <rweddell4@gmail.com>
2018-11-17 12:56:07 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,12 +31,13 @@ engine = Engine.Engine()
 while chosen not in quit_words:
     cls()
     print("WELCOME TO THE WORLD'S GREATEST MOVIE RECOMMENDER")
-    chosen = rawinput("Enter a movie title or type 'exit' to quit:  ")
+    chosen = input("Enter a movie title or type 'exit' to quit:  ")
     recs = []
     try:
         if chosen not in quit_words:
             recs = engine.get_content_recommendations(chosen)
             print(recs.values)
+            print(type(recs))
             for i in range(len(recs)):
                 print(str(i) + '  ' + str(recs[i]))
             print('Would you like to know more about any of these titles?')
@@ -47,9 +48,8 @@ while chosen not in quit_words:
                 if type(more) == int:
                     more = recs[more]
                 print(engine.find_summary(more))
-        larry = input('was it good for you?')
-
-    except KeyError:
+    except KeyError as ke:
+        print(ke)
         print('Sorry, we could not find that movie')
     except ValueError as val:
         print(val)
