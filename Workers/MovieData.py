bcbfb4a425d8beb9f083f6f334a343a2c4882628
David Beck <davidernestbeck@gmail.com>
2018-11-18 16:59:21 -0600

    Lots done

--- Workers/MovieData.py
+++ Workers/MovieData.py
@@ -15,8 +15,6 @@ class MovieData:
         self.data_path = os.path.join(os.getcwd(), 'DataStorage')
         self.data_unzip()
         self.datafile = pd.read_csv(os.path.join(self.data_path, 'movies_metadata.csv'), low_memory=False)
-        self.ratings = pd.read_csv(os.path.join(self.data_path, 'ratings_small.csv'), low_memory=False)[
-            ['userId', 'movieId', 'rating']]
         # Create reduced dimension data set & cosine similarity matrix
         self.data, self.cosine_sim, self.tfidf_matrix = self.preprocess()
 
@@ -25,6 +23,18 @@ class MovieData:
 
     def preprocess(self):
         # TODO: at least get target variables from data, but clean if necessary & reduce dimension size
+        self.minimize_data()
+        self.get_keywords()
+        self.process_text()
+        self.stem_words()
+        cosine_sim, tfidf_matrix = self.calculate_cosine_sim()
+        data = self.datafile
+        print(data.shape)
+        print(data['genres'])
+        print(data['tagline'])
+        print(data['overview'])
+        print(data[data['title'] == 'Batman'])
+        """""
         # Calculate the minimum number of votes required to be in the chart (90th percentile)
         min_votes = self.datafile['vote_count'].quantile(0.90)
         # Calculate mean average vote across entire dataset ala IMDB
@@ -81,15 +91,76 @@ class MovieData:
         tfidf_matrix = tfidf.fit_transform(data['wordsalad'])
         # Compute the cosine similarity matrix
         cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+        """
         return data, cosine_sim, tfidf_matrix
 
+    def minimize_data(self):
+        # Calculate the minimum number of votes required to be in the chart (90th percentile)
+        min_votes = self.datafile['vote_count'].quantile(0.90)
+        # Calculate mean average vote across entire dataset ala IMDB
+        mean_score = float(self.datafile['vote_average'].mean())
+        # Filter out all qualified movies into a new DataFrame (about 4555 entries)
+        self.datafile = self.datafile.loc[self.datafile.vote_count >= min_votes].copy()
+        # Append weighted scores to new DataFrame
+        self.datafile['scores'] = self.datafile.apply(lambda x: (x['vote_count'] / (x['vote_count'] + min_votes) *
+                                                                 x['vote_average']) + (min_votes / (min_votes + x['vote_count']) * mean_score), axis=1)
+        # Drop duplicates
+        self.datafile.drop_duplicates(inplace=True)
+        # Reassign indices of data
+        self.datafile.reset_index(drop=True, inplace=True)
+
+    def get_keywords(self):
+        # Convert IDs to int. Required for merging
+        self.datafile['id'] = self.datafile['id'].astype('int')
+        # Get keywords then merge them with movie metadata
+        keywords = pd.read_csv(os.path.join(self.data_path, 'keywords.csv'))
+        keywords['id'] = keywords['id'].astype('int')
+        # Merge keywords into new dataframe
+        self.datafile = self.datafile.merge(keywords, on='id')
+        self.datafile['keywords'] = self.datafile['keywords'].apply(literal_eval)
+        # Strip out 'name' from keywords
+        self.datafile['keywords'] = self.datafile['keywords'].apply(
+            lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+        # Remove keywords that occur only once
+        s = self.datafile.apply(lambda x: pd.Series(x['keywords']), axis=1).stack().reset_index(level=1, drop=True)
+        s.name = 'keyword'
+        s = s.value_counts()
+        d = s[s == 1]
+        self.datafile['keywords'] = self.datafile['keywords'].apply(lambda x: (i for i in x if i not in d))
+
+    def process_text(self):
+        # Replace NaN with an empty string
+        self.datafile['overview'] = self.datafile['overview'].fillna('')
+        self.datafile['tagline'] = self.datafile['tagline'].fillna('')
+        # Strip out 'name' from genres
+        self.datafile['genres'] = self.datafile['genres'].fillna('[]').apply(literal_eval).apply(
+            lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+        # Split up sentences to lists of string values
+        table = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P'))
+        self.datafile['tagline'] = self.datafile['tagline'].apply(lambda x: x.translate(table).split())
+        self.datafile['overview'] = self.datafile['overview'].apply(lambda x: x.translate(table).split())
+
     def stem_words(self):
+        # Create stemmer object
         snowball = SnowballStemmer('english')
-        # TODO: 'self' should be self.data
-        self['keywords'] = self['keywords'].apply(lambda x: [snowball.stem(i) for i in x])
-        self['tagline'] = self['tagline'].apply(lambda x: [snowball.stem(i) for i in x])
-        self['overview'] = self['overview'].apply(lambda x: [snowball.stem(i) for i in x])
-        self['genres'] = self['genres'].apply(lambda x: [snowball.stem(i) for i in x])
+        # Stem the feature words
+        self.datafile['keywords'] = self.datafile['keywords'].apply(lambda x: [snowball.stem(i) for i in x])
+        self.datafile['tagline'] = self.datafile['tagline'].apply(lambda x: [snowball.stem(i) for i in x])
+        self.datafile['overview'] = self.datafile['overview'].apply(lambda x: [snowball.stem(i) for i in x])
+        # self.datafile['genres'] = self.datafile['genres'].apply(lambda x: [snowball.stem(i) for i in x])
+        self.datafile['keywords'] = self.datafile['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+    def calculate_cosine_sim(self):
+        # Create wordsalad for Tfidf evaluation
+        self.datafile['wordsalad'] = self.datafile['overview'] + self.datafile['tagline'] + self.datafile['keywords'] + self.datafile['genres']
+        self.datafile['wordsalad'] = self.datafile['wordsalad'].apply(lambda x: ' '.join(x))
+        # Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+        tfidf = TfidfVectorizer(stop_words='english')
+        # Construct the required TF-IDF matrix by fitting and transforming the data
+        tfidf_matrix = tfidf.fit_transform(self.datafile['wordsalad'])
+        # Compute the cosine similarity matrix
+        cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+        return cosine_sim, tfidf_matrix
 
     def data_unzip(self):
         zip_ref = ZipFile(os.path.join(self.data_path, 'movies_metadata.zip'), 'r')
