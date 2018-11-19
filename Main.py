0a7621a4937b51760506058e0f4fc71fbb9f4dc9
Rob Weddell <rweddell4@gmail.com>
2018-11-19 17:29:59 -0600

    Added comments
    Added documentation to functions

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 from Workers.Display import *
 from Workers.InputHandler import *
 
-# The main script for a machine-learning movie recommender
+# Main script for a machine-learning movie recommender
 
 title = 'find\n- a -\nfilm'
 
@@ -12,18 +12,23 @@ chosen = ''
 
 inputer = InputHandler()
 
+# Main loop
 while chosen not in inputer.quit_words:
     cls()
     print('\nWelcome to\n')
     display_title(title)
     try:
+        # Asks user for what kind of recommendation that they want
         search_type = inputer.get_search_type()
         cls()
         recs = []
         if chosen not in inputer.quit_words:
+            # Processes request
             recs = inputer.handle_input(search_type)
             cls()
+            # Prints results of request
             print(recs.to_string(header=True, justify=all))
+            # If desired, prints wikipedia entry for chosen movie
             inputer.get_more_info(recs)
     except (ValueError, KeyError) as val:
         print(val)
@@ -33,6 +38,7 @@ while chosen not in inputer.quit_words:
         print('\nSorry, a bug got in. Please try again.\n')
     if chosen not in inputer.quit_words:
         cls()
+        # Asks user if they would like to continue, or exit
         chosen = input('\nWould you like to try a new search? \n')
 cls()
 print()
